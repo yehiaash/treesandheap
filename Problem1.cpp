b811e1c82a91e92739a724dd0799637e51479607
Admin <Admin@DESKTOP-IIL7N6G>
2026-05-12 21:22:48 +0300

    Addede delete function for avl

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 #define ll long long
 #define nll "\n"
 using namespace std;
@@ -12,14 +13,15 @@ struct Book
 struct Node
 {
     Book data;
-    Node *left;
-    Node *right;
+    Node* left;
+    Node* right;
+    int height;
 };
 
 class BSTBook
 {
 private:
-    Node *root;
+    Node* root;
 
 public:
     BSTBook()
@@ -29,7 +31,7 @@ public:
 
     void insert(Book book)
     {
-        Node *newNode = new Node;
+        Node* newNode = new Node;
         newNode->data = book;
         newNode->left = nullptr;
         newNode->right = nullptr;
@@ -40,8 +42,8 @@ public:
             return;
         }
 
-        Node *current = root;
-        Node *parent = nullptr;
+        Node* current = root;
+        Node* parent = nullptr;
         while (current != 0)
         {
             parent = current;
@@ -56,11 +58,11 @@ public:
             parent->right = newNode;
     }
 
-    Node *search(int id)
+    Node* search(int id)
     {
 
-        Node *current = root;
-        Node *parent = nullptr;
+        Node* current = root;
+        Node* parent = nullptr;
         while (current != 0)
         {
             parent = current;
@@ -77,8 +79,8 @@ public:
     }
     void deleteBook(int id)
     {
-        Node *current = root;
-        Node *parent = nullptr;
+        Node* current = root;
+        Node* parent = nullptr;
 
         // find node + parent
         while (current != nullptr && current->data.id != id)
@@ -97,8 +99,8 @@ public:
         // CASE 2: two children
         if (current->left != nullptr && current->right != nullptr)
         {
-            Node *successorParent = current;
-            Node *successor = current->right;
+            Node* successorParent = current;
+            Node* successor = current->right;
 
             while (successor->left != nullptr)
             {
@@ -112,7 +114,7 @@ public:
         }
 
         // CASE 1 & 3: one or zero child
-        Node *child = (current->left != nullptr) ? current->left : current->right;
+        Node* child = (current->left != nullptr) ? current->left : current->right;
 
         if (parent == nullptr)
         {
@@ -133,7 +135,7 @@ public:
     {
         display(root);
     }
-    void display(Node *root)
+    void display(Node* root)
     {
         if (root == nullptr)
             return;
@@ -142,7 +144,7 @@ public:
         visit(root);
         display(root->right);
     }
-    void visit(Node *root)
+    void visit(Node* root)
     {
         cout << "------------------------\n";
         cout << "ID     : " << root->data.id << "\n";
@@ -151,36 +153,36 @@ public:
         cout << "------------------------\n\n";
     }
 
-    void printrange(Node* root , int minID, int maxID){
-        if(root == nullptr) return;
+    void printrange(Node* root, int minID, int maxID) {
+        if (root == nullptr) return;
 
-        if (root->data.id > minID){
+        if (root->data.id > minID) {
             printrange(root->left, minID, maxID);
         }
 
-        if(root->data.id >= minID && root->data.id <= maxID){
+        if (root->data.id >= minID && root->data.id <= maxID) {
             visit(root);
         }
 
-        if(root->data.id < maxID){
+        if (root->data.id < maxID) {
             printrange(root->right, minID, maxID);
         }
     }
 
-    void printINrange(int minID, int maxID){
-        printrange(root,minID,maxID);
+    void printINrange(int minID, int maxID) {
+        printrange(root, minID, maxID);
     }
 
-    Node* closest(Node* root, int target){
+    Node* closest(Node* root, int target) {
         Node* closest = root;
-        while(root!= nullptr){
-            if(abs(root->data.id - target) < abs(closest->data.id - target)){
+        while (root != nullptr) {
+            if (abs(root->data.id - target) < abs(closest->data.id - target)) {
                 closest = root;
             }
 
-            if(root->data.id == target)
+            if (root->data.id == target)
                 return root;
-            if(root->data.id > target){
+            if (root->data.id > target) {
                 root = root->left;
             }
             else
@@ -189,7 +191,7 @@ public:
         return closest;
     }
 
-    void closestSearch(int target){
+    void closestSearch(int target) {
         Node* close = closest(root, target);
         if (close == nullptr)
             cout << "Tree is empty\n";
@@ -197,25 +199,233 @@ public:
             visit(close);
     }
 };
-class AVLBook
+class AVLBook 
 {
 private:
-    Node *root;
+    Node* root;
+
+public:
     AVLBook()
     {
         root = nullptr;
     }
+    int height(Node* root)
+    {
+        if (root == nullptr)
+            return 0;
+        return 1 + max(height(root->right), height(root->left));
+    }
+    int getBalancedFactor(Node* root)
+    {
+        if (root == nullptr)
+            return 0;
+        return height(root->left) - height(root->right);
+    }
+    Node* insertavl(Node* node, Book book)
+    {
+        if (node == nullptr)
+        {
+            Node* newNode = new Node;
+            newNode->data = book;
+            newNode->left = newNode->right = nullptr;
+            return newNode;
+        }
 
-public:
-    
+        if (book.id < node->data.id)
+            node->left = insertavl(node->left, book);
+        else if (book.id > node->data.id)
+            node->right = insertavl(node->right, book);
+        // update height here (if you store height)
+        updateHeight(node);
+        int bf = getBalancedFactor(node);
+
+        // LEFT LEFT
+        if (bf > 1 && book.id < node->left->data.id)
+            return rightRotate(node);
+
+        // RIGHT RIGHT
+        if (bf < -1 && book.id > node->right->data.id)
+            return leftRotate(node);
+
+        // LEFT RIGHT
+        if (bf > 1 && book.id > node->left->data.id)
+        {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+
+        // RIGHT LEFT
+        if (bf < -1 && book.id < node->right->data.id)
+        {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+
+        return node;
+    }
+    Node* rightRotate(Node* y)
+    {
+        Node* x = y->left;
+        Node* T2 = x->right;
+
+        // rotation
+        x->right = y;
+        y->left = T2;
+
+        updateHeight(y);
+        updateHeight(x);
+
+        return x; // new root
+    }
+    Node* leftRotate(Node* x)
+    {
+        Node* y = x->right;
+        Node* T2 = y->left;
+
+        // rotation
+        y->left = x;
+        x->right = T2;
+
+        // update heights
+        updateHeight(x);
+        updateHeight(y);
+
+        return y; // new root
+    }
+    void updateHeight(Node* n)
+    {
+        n->height = 1 + max(height(n->left), height(n->right));
+    }
+    Node* deleteavl(Node* node,Book book)
+    {
+        if (node == nullptr) {
+            return nullptr;
+        }
+        if (book.id < node->data.id)
+            node->left = deleteavl(node->left, book);
+        else if (book.id > node->data.id)
+            node->right = deleteavl(node->right, book);
+        else {
+            if (node->left == nullptr || node->right == nullptr) {
+                Node* child = (node->left != nullptr) ? node->left : node->right;
+                if (child == nullptr) {
+                    return nullptr;
+                }
+                else {
+                    node = child;
+                }
+            }
+            Node* successor = node->right;
+
+            while (successor->left != nullptr)
+            {
+                successor = successor->left;
+            }
+            node->data= successor->data;
+            node->right = deleteavl(node->right, successor->data);
 
 
 
-Node *search(int id)
+        }
+
+        updateHeight(node);
+        int factor=getBalancedFactor(node);
+        if (factor > 1 && book.id < node->left->data.id)
+            return rightRotate(node);
+
+        
+        if (factor < -1 && book.id > node->right->data.id)
+            return leftRotate(node);
+
+        if (factor > 1 && book.id > node->left->data.id)
+        {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+
+       
+        if (factor < -1 && book.id < node->right->data.id)
+        {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+
+
+        return node;
+
+    }
+    void display()
+    {
+        display(root);
+    }
+    void display(Node* root)
+    {
+        if (root == nullptr)
+            return;
+
+        display(root->left);
+        visit(root);
+        display(root->right);
+    }
+    void visit(Node* root)
+    {
+        cout << "------------------------\n";
+        cout << "ID     : " << root->data.id << "\n";
+        cout << "Title  : " << root->data.title << "\n";
+        cout << "Author : " << root->data.author << "\n";
+        cout << "------------------------\n\n";
+    }
+
+    void printrange(Node* root, int minID, int maxID) {
+        if (root == nullptr) return;
+
+        if (root->data.id > minID) {
+            printrange(root->left, minID, maxID);
+        }
+
+        if (root->data.id >= minID && root->data.id <= maxID) {
+            visit(root);
+        }
+
+        if (root->data.id < maxID) {
+            printrange(root->right, minID, maxID);
+        }
+    }
+
+    void printINrange(int minID, int maxID) {
+        printrange(root, minID, maxID);
+    }
+
+    Node* closest(Node* root, int target) {
+        Node* closest = root;
+        while (root != nullptr) {
+            if (abs(root->data.id - target) < abs(closest->data.id - target)) {
+                closest = root;
+            }
+
+            if (root->data.id == target)
+                return root;
+            if (root->data.id > target) {
+                root = root->left;
+            }
+            else
+                root = root->right;
+        }
+        return closest;
+    }
+
+    void closestSearch(int target) {
+        Node* close = closest(root, target);
+        if (close == nullptr)
+            cout << "Tree is empty\n";
+        else
+            visit(close);
+    }
+    Node* search(int id)
     {
 
-        Node *current = root;
-        Node *parent = nullptr;
+        Node* current = root;
+        Node* parent = nullptr;
         while (current != 0)
         {
             parent = current;
@@ -231,25 +441,26 @@ Node *search(int id)
         return nullptr;
     }
 };
+
 int main()
 {
     BSTBook tree;
 
     // INSERT
-    tree.insert({10, "C++ Basics", "Bjarne"});
-    tree.insert({5, "DSA", "Mark"});
-    tree.insert({20, "Algorithms", "CLRS"});
-    tree.insert({3, "OOP", "Ali"});
-    tree.insert({7, "Pointers", "John"});
-    tree.insert({15, "Graphs", "Tarjan"});
-    tree.insert({30, "AI", "Russell"});
+    tree.insert({ 10, "C++ Basics", "Bjarne" });
+    tree.insert({ 5, "DSA", "Mark" });
+    tree.insert({ 20, "Algorithms", "CLRS" });
+    tree.insert({ 3, "OOP", "Ali" });
+    tree.insert({ 7, "Pointers", "John" });
+    tree.insert({ 15, "Graphs", "Tarjan" });
+    tree.insert({ 30, "AI", "Russell" });
 
     cout << "\n=== AFTER INSERT ===\n";
     tree.display();
 
     // SEARCH
     cout << "\n=== SEARCH 7 ===\n";
-    Node *res = tree.search(7);
+    Node* res = tree.search(7);
     if (res)
         cout << "Found: " << res->data.title << "\n";
     else
@@ -257,7 +468,7 @@ int main()
 
     // print in rane 
     cout << "\n=== print in range(5,20) ===\n";
-    tree.printINrange(5,20);
+    tree.printINrange(5, 20);
 
     //closestsearch
     cout << "\n=== closest book ID from 5 ===\n";
@@ -278,10 +489,10 @@ int main()
     tree.deleteBook(10);
     tree.display();
 
-    
+
     //closestsearch
     cout << "\n=== closest book ID from 5 ===\n";
     tree.closestSearch(5);
 
-    
+
 }
